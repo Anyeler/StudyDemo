f946571167800630b9aef4b0043cb44a6a86ae54
Anyeler <414116969@qq.com>
2022-06-15 16:08:00 +0800

    修改touchlog

--- OCDemo/ALResponderChain/ALResponderChain/TestClass/ALTapGestureRecognizer.m
+++ OCDemo/ALResponderChain/ALResponderChain/TestClass/ALTapGestureRecognizer.m
@@ -14,6 +14,26 @@
 
 @implementation ALTapGestureRecognizer
 
+#pragma mark - Touch事件
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    NSLog(@"%@\n%s\n%@\n%@\n", self, __FUNCTION__, touches, event);
+    [super touchesBegan:touches withEvent:event];
+}
+
+- (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    NSLog(@"%@\n%s\n%@\n%@\n", self, __FUNCTION__, touches, event);
+    [super touchesMoved:touches withEvent:event];
+}
+
+- (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    NSLog(@"%@\n%s\n%@\n%@\n", self, __FUNCTION__, touches, event);
+    [super touchesEnded:touches withEvent:event];
+}
+
+- (void)touchesCancelled:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    NSLog(@"%@\n%s\n%@\n%@\n", self, __FUNCTION__, touches, event);
+    [super touchesCancelled:touches withEvent:event];
+}
 
 @end
